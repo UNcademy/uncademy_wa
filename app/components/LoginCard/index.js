20a1dcd38c3b61265a0682f0ea99006a4d7c0efb
nromeron <nromeron@unal.edu.co>
2022-10-28 17:57:37 -0500

    @Harold andres, mk me debe tres polas por hacerle el trabajo

--- app/components/LoginCard/index.js
+++ app/components/LoginCard/index.js
@@ -1,34 +1,78 @@
-import React from 'react';
+import React  ,{ useState } from 'react';
+import { login } from '../../graphql/queries';
+import {client} from "../../graphql/apolloClient";
+
+async function getData(inputs) {
+    const { data } = await client.query({
+        query: login,
+        variables: {
+            username: inputs.username,
+			password: inputs.password
+        }
+    });
+    return data;
+}
 
 export const LoginCard = () => {
+
+	const [inputs, setInputs] = useState({
+		username: "",
+		password: ""
+	  });
+	  
+
+	  const changeForm = e => {
+		setInputs({
+		  ...inputs,
+		  [e.target.name]: e.target.value
+		});
+	  };
+
+	  const login = async e =>{
+		e.preventDefault();
+
+		console.log(inputs)
+		getData(inputs).then(res =>{
+			localStorage.setItem('Token', res.login.data.accessToken)
+			if(res.login.statusCode === 200){
+				window.location.href = '/'
+			}
+		} 
+			).catch(err =>{
+			alert(err.message)
+		})
+	  }
+
+
+
 	return (
 		<section className="">
 			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+					<div className="p-6 space-y-4 md:space-y-6 sm:p-8" onChange={changeForm} onSubmit={login}>
 						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 							Autenticación
 						</h1>
 						<form className="space-y-4 md:space-y-6" action="#">
 							<div>
 								<label
-									for="email"
+									htmlFor="username"
 									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 								>
-									Correo electrónico
+									Nombre de usuario (Sin el @unal.edu.co)
 								</label>
 								<input
-									type="email"
-									name="email"
-									id="email"
+									type="text"
+									name="username"
+									id="username"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-									placeholder="username@unal.edu.co"
+									placeholder="username"
 									required=""
 								/>
 							</div>
 							<div>
 								<label
-									for="password"
+									htmlFor="password"
 									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 								>
 									Contraseña
@@ -55,7 +99,7 @@ export const LoginCard = () => {
 									</div>
 									<div className="ml-3 text-sm">
 										<label
-											for="remember"
+											htmlFor="remember"
 											className="text-gray-500 dark:text-gray-300"
 										>
 											Remember me
@@ -71,7 +115,7 @@ export const LoginCard = () => {
 							</div>
 							<button
 								type="submit"
-								className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+								//className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 							>
 								Sign in
 							</button>
