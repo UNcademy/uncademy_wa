fced4fa46a4805e7be674bf2c2d16b848da9d1f3
Andrés Díaz <36974713+andiazo@users.noreply.github.com>
2022-10-28 18:39:43 -0500

    Update app/components/RegisterCard/index.js
    Co-authored-by: jubustamantem <75868064+jubustamantem@users.noreply.github.com>

--- app/components/RegisterCard/index.js
+++ app/components/RegisterCard/index.js
@@ -14,7 +14,7 @@ async function doMutation(registerBody) {
 
 export const RegisterCard = () => {
 	const [username, setUsername] = useState('');
-	const [usertype, setUsertype] = useState();
+	const [usertype, setUsertype] = useState('');
 	const [password, setPassword] = useState();
 	const [fullname, setFullname] = useState();
 	const [idDoc, setIdDoc] = useState();
