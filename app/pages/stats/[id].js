883b1dd559fed6cffb271c23aa304a4b570ba340
jubustamantem <jubustamantem@unal.edu.co>
2022-10-28 18:21:49 -0500

    Graph Carts in Stats View

--- app/pages/stats/[id].js
+++ app/pages/stats/[id].js
@@ -3,6 +3,12 @@ import Head from "next/head";
 import {useRouter} from "next/router";
 import {client} from "../../graphql/apolloClient";
 import {statsByGroup} from "../../graphql/queries";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement} from 'chart.js';
+import {Pie} from "react-chartjs-2";
+import {Doughnut} from "react-chartjs-2";
+import {Bar} from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
 async function getData(id) {
     const {data} = await client.query({
@@ -27,25 +33,100 @@ export default function ClassList() {
         worst_grade: 0
     })
 
+    const dataPP = {
+        labels: ['Attendance Rate', 'Absence Rate'],
+        datasets: [
+            {
+                label: 'participation_percentage',
+                data: [output.participation_percentage, 100-output.participation_percentage],
+                backgroundColor: [
+                    'rgba(169, 194, 217, 1)',
+                    'rgba(166, 20, 20, 0.5)',
+                ],
+                borderColor: [
+                    'rgba(3, 59, 134, 1)',
+                    'rgba(166, 20, 20, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const dataAP = {
+        labels: ['Approved', 'Failed'],
+        datasets: [
+            {
+                label: 'approbation_percentage',
+                data: [output.approbation_percentage, 100-output.approbation_percentage],
+                backgroundColor: [
+                    'rgba(169, 194, 217, 1)',
+                    'rgba(166, 20, 20, 0.5)',
+                ],
+                borderColor: [
+                    'rgba(3, 59, 134, 1)',
+                    'rgba(166, 20, 20, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const dataGrades = {
+        labels: ['Best Grade', 'Worst Grade', 'Average Grade'],
+        datasets: [
+            {
+                label: "grades",
+                data: [output.best_grade, output.worst_grade, output.average_grade],
+                backgroundColor: [
+                    'rgba(169, 194, 217, 1)',
+                    'rgba(166, 20, 20, 0.5)',
+                    'rgba(73, 42, 85, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(3, 59, 134, 1)',
+                    'rgba(166, 20, 20, 1)',
+                    'rgba(73, 42, 85, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const barOptions = {
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+
     useEffect(() => {
         getData(id).then(res => {
             setOutput(res)
         }).catch(err => console.log(""))
     }, [id])
 
-    function displayStats(stat){
+    function displayPercentajes(stat){
         if (stat === "participation_percentage") {
-            return "Porcentaje de participación"
-        } else if (stat === "approbation_percentage"){
-            return "Porcentaje de aprobación"
-        } else if (stat === "average_grade"){
-            return "Nota promedio"
-        } else if (stat === "standard_deviation"){
-            return "Desviación estándar"
-        } else if (stat === "best_grade"){
-            return "Mejor nota"
-        } else if (stat === "worst_grade"){
-            return "Peor nota"
+            return (<tr>
+                <td className="border border-black text-center p-3 bg-ao text-white text-md font-semibold">
+                    <p>Porcentaje de asistencia</p> <br/>
+                </td>
+                <td className="border border-black text-center bg-g p-3">
+                    <b>{output[stat]} %</b>
+                    <Pie className="mt-2" data={dataPP}/>
+                </td>
+            </tr>)
+        } else if (stat === "approbation_percentage") {
+            return (<tr>
+                <td className="border border-black text-center p-3 bg-ao text-white text-md font-semibold">
+                    <p>Porcentaje de aprobación</p> <br/>
+                </td>
+                <td className="border border-black text-center bg-g p-3">
+                    <b>{output[stat]} %</b>
+                    <Doughnut className="mt-2" data={dataAP}/>
+                </td>
+            </tr>)
         }
     }
 
@@ -61,26 +142,70 @@ export default function ClassList() {
                     <h5 className="text-xl font-bold tracking-tight text-gray-900 mx-auto">Estadísticas de la clase #{id}</h5>
                 </div>
                 <table className="table-auto mx-auto mt-6 border-separate border-spacing-2 border border-black">
+                    <thead>
+                        <th className="border border-black text-center p-3 bg-ac text-blue text-md font-semibold" colSpan={2}>
+                            Porcentajes
+                        </th>
+                    </thead>
                     <tbody>
                     {
                         Object.keys(output).map(stat => {
-                            if (stat !== "__typename"){
-                            return(
-                                <tr>
-                                    <td className="border border-black text-center bg-g p-3 bg-ac text-blue text-md font-semibold">
-                                        {
-                                            displayStats(stat)
-                                        }
-                                    </td>
-                                    <td className="border border-black text-center bg-g p-3">
-                                        {
-                                            output[stat]
-                                        }
-                                    </td>
-                                </tr>
-                            )}
+                            return(displayPercentajes(stat))
                         })
                     }
+                    </tbody>
+                </table>
+                <table className="table-auto mx-auto mt-6 border-separate border-spacing-2 border border-black">
+                    <thead>
+                    <th className="border border-black text-center p-3 bg-ac text-blue text-md font-semibold" colSpan={2}>
+                        Medidas de dispersión
+                    </th>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td className="border border-black text-center p-3 bg-ao text-white text-md font-semibold">
+                            Desviación estándar
+                        </td>
+                        <td className="border border-black text-center bg-g p-3">
+                            {output["standard_deviation"]}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table className="table-auto mx-auto mt-6 border-separate border-spacing-2 border border-black">
+                    <thead>
+                    <th className="border border-black text-center p-3 bg-ac text-blue text-md font-semibold" colSpan={3}>
+                        Rango de calificaciones
+                    </th>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td className="border border-black text-center p-3 bg-ao text-white text-md font-semibold">
+                            Mejor nota
+                        </td>
+                        <td className="border border-black text-center p-3 bg-ao text-white text-md font-semibold">
+                            Peor nota
+                        </td>
+                        <td className="border border-black text-center p-3 bg-ao text-white text-md font-semibold">
+                            Nota promedio
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="border border-black text-center bg-g p-3">
+                            {output["best_grade"]}
+                        </td>
+                        <td className="border border-black text-center bg-g p-3">
+                            {output["worst_grade"]}
+                        </td>
+                        <td className="border border-black text-center bg-g p-3">
+                            {output["average_grade"]}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="border border-black text-center bg-g p-3" colSpan={3}>
+                            <Bar className="mt-2" data={dataGrades} options={barOptions}/>
+                        </td>
+                    </tr>
                     </tbody>
                 </table>
             </main>
