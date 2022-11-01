cf111c017845a9e4f0e0cdfccc359b8d4f981163
jubustamantem <jubustamantem@unal.edu.co>
2022-11-01 10:59:46 -0500

    Docker and proxy added

--- app/graphql/apolloClient.js
+++ app/graphql/apolloClient.js
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'http://172.17.0.1:5000/graphql',
+    uri: 'http://172.17.0.1/graphql',
     cache: new InMemoryCache(),
 })
 
